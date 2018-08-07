e497971805d545a34050c48f7d0328d944001e33
Olga <shpyakina.olga@mail.ru>
2018-08-07 17:53:33 +0700

    fourth commit

--- docs/conf.py
+++ docs/conf.py
@@ -133,7 +133,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'uElecardBoro.tex', 'u\'Elecard Boro: Часто задаваемые вопросы\' Documentation',
+    (master_doc, 'uElecardBoro.tex', 'Elecard Documentation',
      'Elecard', 'manual'),
 ]
 
